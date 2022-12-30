8babc5aa628dbcdecabf38d74e8ad45ebc80312d
unknown <bousilmustapha@gmail.com>
2022-12-30 13:18:48 +0100

    connect controller data

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,35 @@
+//@desc get goals
+//@route GET/api/goals
+//@access Private 
+const getGoals=(req,res)=>{
+    res.status(200).json({message:'Get goals'})
+}
+
+
+//@desc set goal
+//@route POST/api/goals
+//@access Private 
+const setGoal=(req,res)=>{
+    res.status(200).json({message:'set goal'})
+}
+
+
+
+//@desc update goal
+//@route PUT/api/goals/:id 
+//@access Private 
+const updateGoal=(req,res)=>{
+    res.status(200).json({message:`update goal  ${req.params.id}`})
+}
+
+//@desc delete goal
+//@route DELETE/api/goals/:id 
+//@access Private 
+const deleteGoal=(req,res)=>{
+    res.status(200).json({message:`delete goal  ${req.params.id}`})
+}
+
+
+module.exports={
+    getGoals,setGoal,updateGoal,deleteGoal
+}
