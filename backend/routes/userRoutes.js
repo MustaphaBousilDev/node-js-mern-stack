47954b3ed0d9d742fbb991454a8c4c34545e1113
unknown <bousilmustapha@gmail.com>
2022-12-31 17:20:29 +0100

    Auth Middleware and Protecting Routes & getme() functions

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const router=express.Router();
 const {registerUser,loginUser,getMe}=require('../controllers/userController')
-
+const {protect}=require('../middleware/authMiddleware')
 router.post('/',registerUser)
 router.post('/login',loginUser)
-router.get('/me',getMe)
+
+//need function middleware for this function
+router.get('/me',protect,getMe)
 
 module.exports=router
