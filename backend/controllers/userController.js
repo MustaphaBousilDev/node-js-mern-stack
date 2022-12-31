7ebcb2f620ae1db1488ba34eeaa8bc1d00cacdae
unknown <bousilmustapha@gmail.com>
2022-12-31 16:43:21 +0100

    Generate Token in login and signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,7 +32,8 @@ const registerUser=asyncHandler(async (req,res)=>{
         res.status(201).json({
             _id:user.id,
             name:user.name,
-            email:user.email
+            email:user.email,
+            token:generateToken(user._id)
         })
     }else{   
         res.status(400)
@@ -52,7 +53,8 @@ const loginUser=asyncHandler(async (req,res)=>{
         res.json({
             _id:user.id ,
             name:user.name ,
-            email:user.email 
+            email:user.email ,
+            token:generateToken(user._id)
         })
     }else{
         res.status(400)
@@ -67,6 +69,16 @@ const loginUser=asyncHandler(async (req,res)=>{
 const getMe=asyncHandler(async (req,res)=>{
     res.json({message:'fucking get user data'})
 })
+
+
+
+//Generate JWT
+const generateToken=(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+        expiresIn:'30d'
+    })
+}
+
 
 
 module.exports={registerUser,loginUser,getMe}
