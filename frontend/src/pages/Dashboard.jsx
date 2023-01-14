0ece022a9c6d8f3f18501c6ad603e6423e3a1c4e
MustaphaBousilDev <bousilmustapha@gmail.com>
2023-01-14 16:15:43 +0100

    go to dashboard if only  user connect

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 const Dashboard = () => {
-  return (
+  const navigate=useNavigate()
+  const {user}=useSelector((state)=>state.auth)
+  useEffect(()=>{
+    if(!user){
+      navigate('/login')
+    }
+  },[user,navigate])
+  return (  
     <div>
       Dashboard Page 
     </div>
